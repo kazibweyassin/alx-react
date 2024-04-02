6075b639b8ef53ccba3840cfbede991e7f42951c
kazibweyassin <kazibweusama@gmail.com>
2024-04-02 21:38:28 +0300

    new file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -8,14 +8,13 @@ import _ from 'lodash';
 //append elements to the body
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button id="mybutton">Click here to get started</button>');
-$('body').append('<p id="count">0</p>');
+$('body').append(`<button>Click here to get started</button>`);
+$('body').append(`<p id="count"></p>`);
 $('body').append('<p>Copyright - Holberton School</p>');
 
 
 //bind lodash's debounce function to the button
-$('button').on('click', _.debounce(updateCounter, 500, { 'leading': true, 'trailing': false }));
-
+$('button').on('click', _.debounce(updateCounter, 500));
 let count = 0;
 //update the counter
 function updateCounter() {
