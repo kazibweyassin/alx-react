4383736fb5764ad5764b7562c9e4eb37b75ed91d
kazibweyassin <kazibweusama@gmail.com>
2024-04-02 21:43:26 +0300

    new

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	mode: 'production',
-	entry: './js/dashboard_main.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'public')
-	}
+    mode: 'production',
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+    ],
 };
