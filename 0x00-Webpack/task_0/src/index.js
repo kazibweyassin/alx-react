588761db68c92e99482c9564c2d47d52c6c4ad02
kazibweyassin <kazibweusama@gmail.com>
2024-04-02 20:27:00 +0300

    task one

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,15 +1,6 @@
 import $ from 'jquery';
 
-$(document).ready(function() {
-    //create and append first paragraph
-    let paragraph1 = $('<p>').text('Holberton Dashboard');
-    $('body').append(paragraph1);
-
-    //create and append second paragraph
-    let paragraph2 = $('<p>').text('Dashboard data for the students');
-    $('body').append(paragraph2);
-
-    //create and append third paragraph
-    let paragraph3 = $('<p>').text('Copyright - Holberton School');
-    $('body').append(paragraph3);
-});
+//add three diffirence paragraphs to the body
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
