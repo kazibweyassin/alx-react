cebcb46e364fbcfc32a477621d2fb291613f6301
kazibweyassin <kazibweusama@gmail.com>
2024-04-25 22:13:54 +0300

    Seq

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,13 @@
+import { Seq } from "immutable";
+
+function printBestStudents(grades) {
+    const grades = {
+    1: {
+        score: 99,
+        firstName: "guillaume",
+        lastName: "salva",
+    },
+};
+}
+
+export default printBestStudents;
